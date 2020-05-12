5c9058ab1d6a89603885e4e3c9a9369ede32b155
ryanyogan <ryanyogan@gmail.com>
2020-05-12 10:05:47 -0700

    pretty much how apollo 3 client works under the hood

---
+++ src/lib/api/index.ts
@@ -0,0 +1,3 @@
+export * from "./server";
+export * from "./useMutation";
+export * from "./useQuery";
---
+++ src/sections/Listings/Listings.tsx
@@ -0,0 +1,91 @@
+import React from "react";
+import { useMutation, useQuery } from "../../lib/api";
+import {
+  DeleteListingData,
+  DeleteListingVariables,
+  ListingsData,
+} from "./types";
+
+interface Props {
+  title: string;
+}
+
+const LISTINGS = `
+  query Listings {
+    listings {
+      id
+      title
+      image
+      address
+      price
+      numOfGuests
+      numOfBeds
+      numOfBaths
+      rating
+    }
+  }
+`;
+
+const DELETE_LISTING = `
+  mutation DeleteListing($id: ID!) {
+    deleteListing(id: $id) {
+      id
+    }
+  }
+`;
+
+export const Listings = ({ title }: Props) => {
+  const { data, loading, error, refetch } = useQuery<ListingsData>(LISTINGS);
+
+  const [
+    deleteListing,
+    { loading: deleteListingLoading, error: deleteListingError },
+  ] = useMutation<DeleteListingData, DeleteListingVariables>(DELETE_LISTING);
+
+  const handleDeleteListing = async (id: string) => {
+    await deleteListing({ id });
+    refetch();
+  };
+
+  const listings = data ? data.listings : null;
+
+  const listingsList = listings ? (
+    <ul>
+      {listings.map((listing) => {
+        return (
+          <li key={listing.id}>
+            {listing.title}{" "}
+            <button onClick={() => handleDeleteListing(listing.id)}>
+              Delete
+            </button>
+          </li>
+        );
+      })}
+    </ul>
+  ) : null;
+
+  if (loading) {
+    return <h2>Loading...</h2>;
+  }
+
+  if (error) {
+    return <h2>Uh oh! Something went wrong - please try again later :-(</h2>;
+  }
+
+  const deleteListingLoadingMessage = deleteListingLoading ? (
+    <h4>Deletion in progres...</h4>
+  ) : null;
+
+  const deleteListingErrorMessage = deleteListingError ? (
+    <h4>Uh oh! Something went wrong with deleting :( Please try again soon.</h4>
+  ) : null;
+
+  return (
+    <div>
+      <h2>{title}</h2>
+      {listingsList}
+      {deleteListingLoadingMessage}
+      {deleteListingErrorMessage}
+    </div>
+  );
+};
