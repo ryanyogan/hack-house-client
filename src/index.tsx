e2251145e98251965b6c86687ff7841a3002b1c8
ryanyogan <ryanyogan@gmail.com>
2020-05-12 15:25:42 -0700

    compelted basics

--- src/index.tsx
+++ src/index.tsx
@@ -4,17 +4,16 @@ import React from "react";
 import { render } from "react-dom";
 import { Listings } from "./sections/Listings";
 import * as serviceWorker from "./serviceWorker";
+import "./styles/index.css";
 
 const client = new ApolloClient({
   uri: "/api",
 });
 
 render(
-  <React.StrictMode>
-    <ApolloProvider client={client}>
-      <Listings title="HackHouse Listings" />
-    </ApolloProvider>
-  </React.StrictMode>,
+  <ApolloProvider client={client}>
+    <Listings title="HackHouse Listings" />
+  </ApolloProvider>,
   document.getElementById("root")
 );
 
