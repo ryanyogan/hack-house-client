37e822a38838b8a196b00c89465c85073d619712
ryanyogan <ryanyogan@gmail.com>
2020-05-17 10:08:50 -0700

    user profile, and additional details complete

--- src/sections/User/index.tsx
+++ src/sections/User/index.tsx
@@ -1,3 +1,63 @@
+import { useQuery } from "@apollo/react-hooks";
+import { Col, Layout, Row } from "antd";
 import React from "react";
+import { RouteComponentProps } from "react-router-dom";
+import { ErrorBanner, PageSkeleton } from "../../lib/components";
+import { USER } from "../../lib/graphql/queries";
+import {
+  User as UserData,
+  UserVariables,
+} from "../../lib/graphql/queries/User/__generated__/User";
+import { Viewer } from "../../lib/types";
+import { UserProfile } from "./components";
 
-export const User = () => <h2>Million dollar idea...</h2>;
+interface Props {
+  viewer: Viewer;
+}
+
+interface MatchParams {
+  id: string;
+}
+
+const { Content } = Layout;
+
+export const User = ({
+  viewer,
+  match,
+}: Props & RouteComponentProps<MatchParams>) => {
+  const { data, loading, error } = useQuery<UserData, UserVariables>(USER, {
+    variables: { id: match.params.id },
+  });
+
+  if (loading) {
+    return (
+      <Content className="user">
+        <PageSkeleton />
+      </Content>
+    );
+  }
+
+  if (error) {
+    return (
+      <Content className="user">
+        <ErrorBanner description="This user may not exist or we've encountered an error.  Please try again soon." />
+        <PageSkeleton />
+      </Content>
+    );
+  }
+
+  const viewerIsUser = viewer.id === match.params.id;
+
+  const userProfileElement =
+    data && data.user ? (
+      <UserProfile user={data.user} viewerIsUser={viewerIsUser} />
+    ) : null;
+
+  return (
+    <Content className="user">
+      <Row gutter={12} type="flex" justify="space-between">
+        <Col xs={24}>{userProfileElement}</Col>
+      </Row>
+    </Content>
+  );
+};
